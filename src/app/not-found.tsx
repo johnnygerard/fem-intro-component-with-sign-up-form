a7d38c9124c2fe2dc3f6ab0dc927987d2e95cdd1
Johnny Gérard <john@jgerard.dev>
2025-03-27 00:42:12 +0100

    feat: style not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,10 +8,19 @@ export const metadata: Metadata = {
 
 const NotFound = () => {
   return (
-    <div className="text-center">
-      <h1>404 Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <Link href="/">Back to Home</Link>
+    <div className="flex flex-col items-center justify-center gap-6 p-8 text-center text-white">
+      <h1 className="text-[3.125rem]/[3.4375rem] font-bold -tracking-[0.03256rem]">
+        404 Not Found
+      </h1>
+      <p className="text-[1rem]/[1.625rem] font-medium">
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <Link
+        href="/"
+        className="grid place-items-center rounded-[0.625rem] bg-[#5E54A4] px-16 py-4 text-[0.9375rem]/[1.625rem] tracking-[0.01675rem] text-white shadow-[0_0.5rem_rgba(0,0,0,0.15)] transition duration-300 ease-in-out hover:bg-[#4a3e85]"
+      >
+        Back to Home
+      </Link>
     </div>
   );
 };
