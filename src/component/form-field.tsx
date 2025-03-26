9ecfaf0fc2012af899e14df542a518b535475115
Johnny Gérard <john@jgerard.dev>
2025-03-26 19:22:50 +0100

    refactor: add Boolean variable isErrorVisible

--- src/component/form-field.tsx
+++ src/component/form-field.tsx
@@ -25,6 +25,7 @@ export const FormField = memo(
   ({ className, isSubmitted, placeholder, validate, ...props }: Props) => {
     const [validationMessage, setValidationMessage] = useState("");
     const inputRef = useRef<HTMLInputElement>(null);
+    const isErrorVisible = validationMessage !== "";
 
     const updateValidation = useCallback(() => {
       const input = inputRef.current;
@@ -45,13 +46,13 @@ export const FormField = memo(
             ref={inputRef}
             onBlur={() => updateValidation()}
             onChange={() => updateValidation()}
-            placeholder={validationMessage ? "" : placeholder}
+            placeholder={isErrorVisible ? "" : placeholder}
             className={cn(
               "text-[0.875rem]/[1.625rem] font-semibold tracking-[0.01563rem]",
               "text-[#3D3B48] transition-colors placeholder:text-[#3D3B48]/75",
               "h-14 rounded-[0.3125rem] bg-white px-5 outline-none",
-              validationMessage && "pr-17 text-[#FF7979]",
-              validationMessage
+              isErrorVisible && "pr-17 text-[#FF7979]",
+              isErrorVisible
                 ? "border-2 border-[#FF7979]"
                 : "border border-[#DEDEDE] focus-visible:border-[#5E54A4]",
               "w-full",
@@ -59,11 +60,11 @@ export const FormField = memo(
             )}
             {...props}
           />
-          {validationMessage && (
+          {isErrorVisible && (
             <IconError className="absolute top-1/2 right-6 -translate-y-1/2 animate-fade-in" />
           )}
         </div>
-        {validationMessage && (
+        {isErrorVisible && (
           <p
             className={cn(
               "mt-1.5 animate-fade-in text-right",
